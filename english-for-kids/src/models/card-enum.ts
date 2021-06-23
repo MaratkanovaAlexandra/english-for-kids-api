009bca980c1cf0cbc83bcd059ee40f55607fa2e0
Maratkanova Alexandra <ola.maratkanova@gmail.com>
2021-06-23 16:00:01 +0400

    fear: create cards enum

---
+++ english-for-kids/src/models/card-enum.ts
@@ -0,0 +1,18 @@
+import * as Const from './const';
+
+const CardEnum = {
+    [Const.MAIN_PAGE]: [
+        {name: Const.ACTION_A, img: 'https://i0.wp.com/post.healthline.com/wp-content/uploads/2021/03/running-1296x728-header.jpg?w=1155&h=1528'},
+        {name: Const.ACTION_B, img: 'https://m.media-amazon.com/images/S/aplus-media/sc/64ff7002-e54e-4dd4-bfab-bb1760582806.__CR0,0,970,600_PT0_SX970_V1___.jpg'},
+        {name: Const.ACTION_C, img: 'https://c8.alamy.com/comp/2C9XG56/children-jump-on-the-bed-2C9XG56.jpg'},
+        {name: Const.ADJECTIVE, img: 'https://s2.coinmarketcap.com/static/img/coins/200x200/9824.pngs'},
+        {name: Const.ANIMAL_A, img: 'https://media.wired.com/photos/593261cab8eb31692072f129/master/pass/85120553.jpg'},
+        {name: Const.ANIMAL_B, img: 'https://aldf.org/wp-content/uploads/2018/05/lamb-iStock-665494268-16x9-e1559777676675-1200x675.jpg'},
+        {name: Const.CLOTHES, img: 'https://www.thespruce.com/thmb/UHAgJr-Im4UfgiGFJYu_OChEVRM=/1333x1000/smart/filters:no_upscale()/prevent-clothes-from-shrinking-and-stretching-2146151-06-fa51daf7a5f444dd9689e5ccaa3f8b08.jpg'},
+        {name: Const.EMOTION, img: 'https://d1urgxgdb4lky3.cloudfront.net/wp-content/uploads/2021/04/KCkids.jpg'}
+    ]
+
+
+}
+
+export default CardEnum;
