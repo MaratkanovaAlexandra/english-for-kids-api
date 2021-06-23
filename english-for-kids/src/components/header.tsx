82af32505b58cb77552ab83bae9aee7479df1280
Maratkanova Alexandra <ola.maratkanova@gmail.com>
2021-06-23 13:21:58 +0400

    feat: add state change on click

--- english-for-kids/src/components/header.tsx
+++ english-for-kids/src/components/header.tsx
@@ -6,32 +6,45 @@ export interface HeaderProps {
 }
  
 export interface HeaderState {
-    playMode?: boolean;
+    playMode: boolean;
     menuMode?: boolean;
 }
  
 class Header extends React.Component <Readonly<HeaderProps>, HeaderState> {
     state = {
-       
+        playMode: this.props.playMode === undefined? false : this.props.playMode,  
+        menuMode: this.props.menuMode === undefined? false : this.props.menuMode,  
     }
     
     render() { 
         return (  
             <header className = 'header'>
-                <div className = {this.getHamburger()}></div>
-                <div className = {this.getMode()}></div>
+                <div className = {this.getHamburger()} onClick = {this.hambergerClick}></div>
+                <div className = {this.getMode()} onClick = {this.modeClick}></div>
             </header>
         );
     }
 
     private getHamburger() {
         let styles = 'header__hamburger-';
-        return this.props.menuMode? styles + 'open' : styles + 'closed' 
+        return this.state.menuMode? styles + 'open' : styles + 'closed'; 
     }
 
     private getMode() {
         let styles = 'header__mode-';
-        return this.props.playMode? styles + 'play' : styles + 'train';
+        return this.state.playMode? styles + 'play' : styles + 'train';
+    }
+
+    private hambergerClick = () => {
+        this.setState({
+            menuMode: !this.state.menuMode
+        });
+    }
+
+    private modeClick = () => {
+        this.setState({
+            playMode: !this.state.playMode
+        });
     }
 }
  
