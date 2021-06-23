4ec8a0dd92fff052018182954a79ef37699cf6ec
Maratkanova Alexandra <ola.maratkanova@gmail.com>
2021-06-23 13:32:11 +0400

    feat: add consts

---
+++ english-for-kids/src/models/const.ts
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+// Side Bar 
+export const MAIN_PAGE = "Main Page";
+export const ACTION_A = "Action (set A)";
+export const ACTION_B = "Action (set B)";
+export const ACTION_C = "Action (set C)";
+export const ADJECTIVE = "Adjective";
+export const ANIMAL_A = "Animal (set A)";
+export const ANIMAL_B = "Animal (set B)";
+export const CLOTHES = "Clothes";
+export const EMOTION = "Emotion";
