7dc054dd9462544f06390945ade3d75556ecf36f
Maratkanova Alexandra <ola.maratkanova@gmail.com>
2021-06-23 17:38:55 +0400

    feat: add main page card

--- english-for-kids/src/components/card.tsx
+++ english-for-kids/src/components/card.tsx
@@ -1,7 +1,9 @@
 import React, { PureComponent } from 'react';
 
 export interface CardProps {
+    playMode: boolean,
     name: string,
+    transl?: string,
     img: string
 }
  
@@ -11,12 +13,41 @@ export interface CardState {
  
 class Card extends PureComponent<CardProps, CardState> {
     render() { 
+        return this.getCardType();
+    }
+
+    private getCardType = () => {
+        if (this.props.transl === undefined) return this.getMainPageCard();
+        return this.getBaseCard();
+    }
+
+    private getBaseCard = () => {
         return ( 
             <div className = {"card"}>
-                <img className = {"card__img"} src={this.props.img} alt={this.props.name} />
-                <p className = {"card__eng"}>{this.props.name}</p>
+                <div className = {"card__top"}>
+                    <img className = {"card__img"} src={this.props.img} alt={this.props.name} />
+                    <p className = {"card__eng"}>{this.props.name}</p>
+                </div>
+                <div className = {"card__back"}>
+                    <p className = {"card__eng"}>{this.props.transl}</p>
+                </div>
             </div>
         );
+    }
+
+    private getMainPageCard = () => {
+        return ( 
+            <div className = {"card"}>
+                <div className = {this.getBackgroundColor()}></div>
+                <img className = {"card__circleImg"} src={this.props.img} alt={this.props.name} />
+                <p className = {"card__title"}>{this.props.name}</p>
+            </div>
+        ); 
+    }
+
+    private getBackgroundColor = () => {
+        let styles = "card__background ";
+        return this.props.playMode ? styles + "play" : styles + "train";
     }
 }
  
