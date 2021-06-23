a5206ad6b7cd1c677d8690afdb57e9dd183eb0ca
Maratkanova Alexandra <ola.maratkanova@gmail.com>
2021-06-23 15:10:02 +0400

    feat: add event handling in header

--- english-for-kids/src/components/header.tsx
+++ english-for-kids/src/components/header.tsx
@@ -1,50 +1,31 @@
-import React, { PureComponent } from 'react';
+import React, { MouseEventHandler, PureComponent } from 'react';
 
 export interface HeaderProps {
     playMode?: boolean;
     menuMode?: boolean;
+    functions: { play: Function, menu: Function}
 }
  
-export interface HeaderState {
-    playMode: boolean;
-    menuMode?: boolean;
-}
+export interface HeaderState {}
  
-class Header extends React.Component <Readonly<HeaderProps>, HeaderState> {
-    state = {
-        playMode: this.props.playMode === undefined? false : this.props.playMode,  
-        menuMode: this.props.menuMode === undefined? false : this.props.menuMode,  
-    }
-    
+class Header extends PureComponent <Readonly<HeaderProps>, HeaderState> {
     render() { 
         return (  
             <header className = 'header'>
-                <div className = {this.getHamburger()} onClick = {this.hambergerClick}></div>
-                <div className = {this.getMode()} onClick = {this.modeClick}></div>
+                <div className = {this.getHamburger()} onClick = {this.props.functions.menu as MouseEventHandler} ></div>
+                <div className = {this.getMode()} onClick = {this.props.functions.play as MouseEventHandler}></div>
             </header>
         );
     }
 
     private getHamburger() {
         let styles = 'header__hamburger-';
-        return this.state.menuMode? styles + 'open' : styles + 'closed'; 
+        return this.props.menuMode? styles + 'open' : styles + 'closed'; 
     }
 
     private getMode() {
         let styles = 'header__mode-';
-        return this.state.playMode? styles + 'play' : styles + 'train';
-    }
-
-    private hambergerClick = () => {
-        this.setState({
-            menuMode: !this.state.menuMode
-        });
-    }
-
-    private modeClick = () => {
-        this.setState({
-            playMode: !this.state.playMode
-        });
+        return this.props.playMode? styles + 'play' : styles + 'train';
     }
 }
  
