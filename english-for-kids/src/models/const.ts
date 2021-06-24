843080f9f36a35e3f062f7b525e54140230babe7
Maratkanova Alexandra <ola.maratkanova@gmail.com>
2021-06-24 10:57:53 +0400

    feat: add  login in conct

--- english-for-kids/src/models/const.ts
+++ english-for-kids/src/models/const.ts
@@ -1,5 +1,3 @@
-import * as React from 'react';
-
 // Side Bar 
 export const MAIN_PAGE = "Main Page";
 export const ACTION_A = "Action (set A)";
@@ -10,3 +8,5 @@ export const ANIMAL_A = "Animal (set A)";
 export const ANIMAL_B = "Animal (set B)";
 export const CLOTHES = "Clothes";
 export const EMOTION = "Emotion";
+
+export const LOGIN = "Login";
