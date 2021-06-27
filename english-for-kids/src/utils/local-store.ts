af3fe9f5df341321196869f7eb8c098ce7517611
Maratkanova Alexandra <ola.maratkanova@gmail.com>
2021-06-27 20:20:08 +0400

    feat: add clean losal storage function

--- english-for-kids/src/utils/local-store.ts
+++ english-for-kids/src/utils/local-store.ts
@@ -1,13 +1,22 @@
 import CardEnum from "../models/card-enum";
 import * as Const from "../models/const";
 
-export async function createLocalStorage() {
+export function createLocalStorage() {
     for (const key in CardEnum) {
         if (key !== Const.MAIN_PAGE)
         CardEnum[key].forEach((card) => {
             if (localStorage[`${card.name}`] === undefined) {
                 localStorage.setItem(card.name, JSON.stringify({trainClick: 0, correctClick: 0, wrongClick: 0}));
             }
+        })
+    }
+}
+
+export function cleanLocalStorage() {
+    for (const key in CardEnum) {
+        if (key !== Const.MAIN_PAGE)
+        CardEnum[key].forEach((card) => {
+            localStorage.setItem(card.name, JSON.stringify({trainClick: 0, correctClick: 0, wrongClick: 0}));
         })
     }
 }
