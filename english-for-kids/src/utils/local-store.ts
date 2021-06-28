81985ae1ec97c389384274f014f11a8d7c3ff9d1
Maratkanova Alexandra <ola.maratkanova@gmail.com>
2021-06-28 17:04:40 +0400

    fix: check losal-store with eslint

--- english-for-kids/src/utils/local-store.ts
+++ english-for-kids/src/utils/local-store.ts
@@ -2,23 +2,25 @@ import CardEnum from '../models/card-enum';
 import * as Const from '../models/const';
 
 export function createLocalStorage() {
-  for (const key in CardEnum) {
-    if (key !== Const.MAIN_PAGE)
+  Object.keys(CardEnum).forEach((key) => {
+    if (key !== Const.MAIN_PAGE) {
       CardEnum[key].forEach((card) => {
         if (localStorage[`${card.name}`] === undefined) {
-          localStorage.setItem(card.name, JSON.stringify({ trainClick: 0, correctClick: 0, wrongClick: 0 }));
+          localStorage
+            .setItem(card.name, JSON.stringify({ trainClick: 0, correctClick: 0, wrongClick: 0 }));
         }
       });
-  }
+    }
+  });
 }
 
 export function cleanLocalStorage() {
-  for (const key in CardEnum) {
+  Object.keys(CardEnum).forEach((key) => {
     if (key !== Const.MAIN_PAGE) {
       CardEnum[key].forEach((card) => {
         localStorage.setItem(card.name,
           JSON.stringify({ trainClick: 0, correctClick: 0, wrongClick: 0 }));
       });
     }
-  }
+  });
 }
