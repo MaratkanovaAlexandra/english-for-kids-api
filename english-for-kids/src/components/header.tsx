702f3b2e74eae959c4b2589c0eb2248c00fb47bf
Maratkanova Alexandra <ola.maratkanova@gmail.com>
2021-06-28 19:44:27 +0400

    fix: check header with eslint

--- english-for-kids/src/components/header.tsx
+++ english-for-kids/src/components/header.tsx
@@ -1,31 +1,33 @@
-import { MouseEventHandler, PureComponent } from "react";
-import Redux from "./../models/redux";
+import React, { MouseEventHandler, PureComponent } from "react";
+import Redux from "../models/redux";
 
 export interface HeaderProps {
     functions: { play: Function, menu: Function}
 }
- 
+
 export interface HeaderState {}
- 
-class Header extends PureComponent <Readonly<HeaderProps>, HeaderState> {
-    render() { 
-        return (  
-            <header className = "header">
-                <div className = {this.getHamburger()} onClick = {this.props.functions.menu as MouseEventHandler} ></div>
-                <div className = {this.getMode()} onClick = {this.props.functions.play as MouseEventHandler}></div>
-            </header>
-        );
-    }
 
-    private getHamburger() {
-        let styles = "header__hamburger-";
-        return Redux.state.menuMode? styles + "open" : styles + "closed"; 
-    }
+class Header extends PureComponent<Readonly<HeaderProps>, HeaderState> {
+  private getHamburger = Redux.state.menuMode ? "header__hamburger-open" : "header__hamburger-closed";
 
-    private getMode() {
-        let styles = "header__mode-";
-        return Redux.state.playMode? styles + "play" : styles + "train";
-    }
+  private getMode = Redux.state.playMode ? "header__mode-play" : "header__mode-train";
+
+  render() {
+    return (
+      <header className="header">
+        <div
+          role="figure"
+          className={this.getHamburger}
+          onClick={this.props.functions.menu as MouseEventHandler}
+        />
+        <div
+          role="figure"
+          className={this.getMode}
+          onClick={this.props.functions.play as MouseEventHandler}
+        />
+      </header>
+    );
+  }
 }
- 
+
 export default Header;
