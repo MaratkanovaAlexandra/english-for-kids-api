7fcdf781044797370dd4fcaa349a443738c536d6
Maratkanova Alexandra <ola.maratkanova@gmail.com>
2021-06-28 21:02:05 +0400

    fix : function returns null no matte what

--- english-for-kids/src/utils/card-searcher.ts
+++ english-for-kids/src/utils/card-searcher.ts
@@ -11,25 +11,16 @@ function findCard(name: string): {
     img: string;
     sound: null;
 } {
+  let result: { name: string; transl: string; img: string; sound: string }|{
+    name: string; transl: null; img: string; sound: null} = { name: "", transl: null, img: "", sound: null };
   Object.keys(CardEnum).forEach((key) => {
     for (let i = 0; i < CardEnum[key].length; i += 1) {
       if (CardEnum[key][i].name === name) {
-        return CardEnum[key][i];
+        result = CardEnum[key][i];
       }
     }
-    return {
-      name: "",
-      transl: null,
-      img: "",
-      sound: null,
-    };
   });
-  return {
-    name: "",
-    transl: null,
-    img: "",
-    sound: null,
-  };
+  return result;
 }
 
 export default findCard;
