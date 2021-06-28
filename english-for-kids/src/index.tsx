30335dd251bd683b8fdb9d223c1995569308fa1e
Maratkanova Alexandra <ola.maratkanova@gmail.com>
2021-06-28 17:36:12 +0400

    fix: check index with eslint

--- english-for-kids/src/index.tsx
+++ english-for-kids/src/index.tsx
@@ -1,16 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './styles/main.scss';
-import App from './components/app';
-import { createLocalStorage } from './utils/local-store';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./styles/main.scss";
+import App from "./components/app";
+import { createLocalStorage } from "./utils/local-store";
+import reportWebVitals from "./reportWebVitals";
 
 createLocalStorage();
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
