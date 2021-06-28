e971778798a4ff224ba7e5f8300ce02684a42b7a
Maratkanova Alexandra <ola.maratkanova@gmail.com>
2021-06-28 16:08:59 +0400

    fix: eslint

--- english-for-kids/src/utils/local-store.ts
+++ english-for-kids/src/utils/local-store.ts
@@ -1,22 +1,24 @@
-import CardEnum from "../models/card-enum";
-import * as Const from "../models/const";
+import CardEnum from '../models/card-enum';
+import * as Const from '../models/const';
 
 export function createLocalStorage() {
-    for (const key in CardEnum) {
-        if (key !== Const.MAIN_PAGE)
-        CardEnum[key].forEach((card) => {
-            if (localStorage[`${card.name}`] === undefined) {
-                localStorage.setItem(card.name, JSON.stringify({trainClick: 0, correctClick: 0, wrongClick: 0}));
-            }
-        })
-    }
+  for (const key in CardEnum) {
+    if (key !== Const.MAIN_PAGE)
+      CardEnum[key].forEach((card) => {
+        if (localStorage[`${card.name}`] === undefined) {
+          localStorage.setItem(card.name, JSON.stringify({ trainClick: 0, correctClick: 0, wrongClick: 0 }));
+        }
+      });
+  }
 }
 
 export function cleanLocalStorage() {
-    for (const key in CardEnum) {
-        if (key !== Const.MAIN_PAGE)
-        CardEnum[key].forEach((card) => {
-            localStorage.setItem(card.name, JSON.stringify({trainClick: 0, correctClick: 0, wrongClick: 0}));
-        })
+  for (const key in CardEnum) {
+    if (key !== Const.MAIN_PAGE) {
+      CardEnum[key].forEach((card) => {
+        localStorage.setItem(card.name,
+          JSON.stringify({ trainClick: 0, correctClick: 0, wrongClick: 0 }));
+      });
     }
+  }
 }
