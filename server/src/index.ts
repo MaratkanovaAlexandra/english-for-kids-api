ce547a85f10c3cae9fb9894ae08a417aed9f937f
Maratkanova Alexandra <ola.maratkanova@gmail.com>
2021-07-04 12:13:11 +0400

    fix: check index with eslint

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,79 +1,88 @@
-import express from 'express';
-import DATA_BASE from './db';
+import express from "express";
+import DATA_BASE from "./db";
 
 const APP = express();
 const PORT = 3000;
 APP.use(express.json());
-APP.set('Content-Type', "application/json");
+APP.set("Content-Type", "application/json");
 
-APP.get('/:page/:id?', (req, res) => {
+APP.get("/:page/:id?", (req, res) => {
   const RESULT = DATA_BASE[req.params.page];
   if (JSON.stringify(req.query) === "{}" && req.params.id === undefined) {
     res.send(RESULT);
   } else if (req.params.id !== undefined) {
-    const ID = /^\d+$/.test(req.params.id) ? Number(req.params.id) :  req.params.id;
-    const ITEM = (RESULT as {id: number|string}[]).filter(item => item.id === ID)[0];
+    const ID = /^\d+$/.test(req.params.id) ? Number(req.params.id) : req.params.id;
+    const ITEM = (RESULT as {id: number|string}[]).filter((item) => item.id === ID)[0];
     res.send(ITEM);
   } else {
-    const ITEM = (RESULT as {page: string}[]).filter(item => item.page === req.query.page)[0];
+    const ITEM = (RESULT as {page: string}[]).filter((item) => item.page === req.query.page)[0];
     res.send(ITEM);
   }
 });
 
-APP.post('/:page', (req, res) => {
+APP.post("/:page", (req, res) => {
   if (req.params.page === "pages") {
     const PAGE_ID = req.body.name.toLocaleLowerCase().split(" ").join("_");
-    const PAGE_ITEM = {page: req.body.name, id: PAGE_ID};
-    const MAIN_PAGE_ITEM = {id: DATA_BASE.main_page.length+1, name: req.body.name, img:  req.body.img, transl: null, sound: null};
+    const PAGE_ITEM = { page: req.body.name, id: PAGE_ID };
+    const MAIN_PAGE_ITEM = {
+      id: DATA_BASE.main_page.length + 1,
+      name: req.body.name,
+      img: req.body.img,
+      transl: null,
+      sound: null,
+    };
     DATA_BASE.pages.push(PAGE_ITEM);
     DATA_BASE.main_page.push(MAIN_PAGE_ITEM);
     DATA_BASE[PAGE_ID] = {};
   } else {
-    const ITEM = {id: DATA_BASE[req.params.page].length+1, 
-                  name: req.body.name, 
-                  transl: req.body.transl, 
-                  img: req.body.img, 
-                  sound: req.body.sound};
+    const ITEM = {
+      id: DATA_BASE[req.params.page].length + 1,
+      name: req.body.name,
+      transl: req.body.transl,
+      img: req.body.img,
+      sound: req.body.sound,
+    };
     DATA_BASE[req.params.page].push(ITEM);
-    console.log(DATA_BASE[req.params.page]);
   }
   res.sendStatus(200);
 });
 
-APP.put('/:page/:id', (req, res) => {
-  const ID = /^\d+$/.test(req.params.id) ? Number(req.params.id) :  req.params.id;
-  let ITEM = (DATA_BASE[req.params.page] as {id: number|string, page: string}[]).filter(item => item.id === ID)[0];
+APP.put("/:page/:id", (req, res) => {
+  const ID = /^\d+$/.test(req.params.id) ? Number(req.params.id) : req.params.id;
+  const ITEM = (DATA_BASE[req.params.page] as {id: number|string, page: string}[])
+    .filter((item) => item.id === ID)[0];
   if (req.params.page === "pages") {
-    const MAIN_PAGE_ITEM =  (DATA_BASE.main_page as {name: string}[]).filter(item => item.name ===  ITEM.page)[0];
+    const MAIN_PAGE_ITEM = (DATA_BASE.main_page as {name: string}[])
+      .filter((item) => item.name === ITEM.page)[0];
     MAIN_PAGE_ITEM.name = req.body.page;
   }
-  Object.keys(ITEM).forEach(key => {
+  Object.keys(ITEM).forEach((key) => {
     ITEM[key] = req.body[key];
   });
   res.sendStatus(200);
 });
 
-APP.delete('/:page/:id', (req, res) => {
-  const ID = /^\d+$/.test(req.params.id) ? Number(req.params.id) :  req.params.id;
-  let word_id: number = null;
-  for (let item = 0; item < DATA_BASE[req.params.page].length; item++) { 
-    if(DATA_BASE[req.params.page][item].id === ID) {
-      word_id = item;
+APP.delete("/:page/:id", (req, res) => {
+  const ID = /^\d+$/.test(req.params.id) ? Number(req.params.id) : req.params.id;
+  let wordId: number = null;
+  for (let item = 0; item < DATA_BASE[req.params.page].length; item += 1) {
+    if (DATA_BASE[req.params.page][item].id === ID) {
+      wordId = item;
     }
   }
-  if(req.params.page === "pages") {
-    let main_page: number = null;
-    for (let item = 0; item < DATA_BASE.main_page.length; item++) { 
-      if(DATA_BASE.main_page[item].name === DATA_BASE.pages[item].page) {
-        main_page = item;
+  if (req.params.page === "pages") {
+    let mainPage: number = null;
+    for (let item = 0; item < DATA_BASE.main_page.length; item += 1) {
+      if (DATA_BASE.main_page[item].name === DATA_BASE.pages[item].page) {
+        mainPage = item;
       }
     }
-    DATA_BASE.main_page.splice(main_page, 1);
+    DATA_BASE.main_page.splice(mainPage, 1);
   }
-  DATA_BASE[req.params.page].splice(word_id, 1);
+  DATA_BASE[req.params.page].splice(wordId, 1);
   res.sendStatus(200);
 });
 
 APP.listen(PORT, () => {
-  console.log(`Server is running at http://localhost:`, PORT)
-})
+  console.log("Server is running at http://localhost:", PORT);
+});
