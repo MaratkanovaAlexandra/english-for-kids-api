d9194bbd66572091e38655303db9f9ccf6b0532d
Maratkanova Alexandra <ola.maratkanova@gmail.com>
2021-07-05 16:53:49 +0400

    refactor: clean side-bar

--- english-for-kids/src/components/side-bar.tsx
+++ english-for-kids/src/components/side-bar.tsx
@@ -1,4 +1,6 @@
 import React, { MouseEventHandler, PureComponent } from "react";
+// import { getPageByName } from "../utils/fetch-funstions";
+// import { Link } from "react-router-dom";
 import * as Const from "../models/const";
 import Redux from "../models/redux";
 
@@ -31,27 +33,13 @@ class SideBar extends PureComponent<SideBarProps, SideBarState> {
           className="sideBar__items"
           onClick={funstions[0] as MouseEventHandler}
         >
-          <li className={this.getItemStyle(Const.MAIN_PAGE)}>
-            {Const.MAIN_PAGE}
-          </li>
-          <li className={this.getItemStyle(Const.ACTION_A)}>
-            {Const.ACTION_A}
-          </li>
-          <li className={this.getItemStyle(Const.ACTION_B)}>
-            {Const.ACTION_B}
-          </li>
-          <li className={this.getItemStyle(Const.ACTION_C)}>
-            {Const.ACTION_C}
-          </li>
-          <li className={this.getItemStyle(Const.ADJECTIVE)}>
-            {Const.ADJECTIVE}
-          </li>
-          <li className={this.getItemStyle(Const.ANIMAL_A)}>
-            {Const.ANIMAL_A}
-          </li>
-          <li className={this.getItemStyle(Const.ANIMAL_B)}>
-            {Const.ANIMAL_B}
-          </li>
+          <li className={this.getItemStyle(Const.MAIN_PAGE)}>{Const.MAIN_PAGE}</li>
+          <li className={this.getItemStyle(Const.ACTION_A)}>{Const.ACTION_A}</li>
+          <li className={this.getItemStyle(Const.ACTION_B)}>{Const.ACTION_B}</li>
+          <li className={this.getItemStyle(Const.ACTION_C)}>{Const.ACTION_C}</li>
+          <li className={this.getItemStyle(Const.ADJECTIVE)}>{Const.ADJECTIVE}</li>
+          <li className={this.getItemStyle(Const.ANIMAL_A)}>{Const.ANIMAL_A}</li>
+          <li className={this.getItemStyle(Const.ANIMAL_B)}>{Const.ANIMAL_B}</li>
           <li className={this.getItemStyle(Const.CLOTHES)}>{Const.CLOTHES}</li>
           <li className={this.getItemStyle(Const.EMOTION)}>{Const.EMOTION}</li>
           <li className={this.getItemStyle(Const.SCOPE)}>{Const.SCOPE}</li>
