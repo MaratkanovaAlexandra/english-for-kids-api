a1a9f1526ff93e213c2db9301a2a1d677bde8b79
Maratkanova Alexandra <ola.maratkanova@gmail.com>
2021-07-05 12:49:48 +0400

    feat: add get page by name

--- english-for-kids/src/utils/fetch-funstions.ts
+++ english-for-kids/src/utils/fetch-funstions.ts
@@ -1,9 +1,16 @@
 import { MAIN_SERVER } from "../models/url";
 import Page from "../types/page";
 
-export const getPages = async():Promise<Page> => {
+export const getPages = async():Promise<Page[]> => {
   const req = await fetch(`${MAIN_SERVER}/pages`);
   const res = await req.json();
 
   return res;
+}
+
+export const getPageByName = async(page: string):Promise<Page> => {
+    const req = await fetch(`${MAIN_SERVER}/pages?page=${page}`);
+    const res = await req.json();
+
+    return res;
 }
