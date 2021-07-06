0366311e56f7a3c2e950afc78d861067f008bb35
Maratkanova Alexandra <ola.maratkanova@gmail.com>
2021-07-06 20:49:10 +0400

    feat: add route for admin

--- english-for-kids/src/index.tsx
+++ english-for-kids/src/index.tsx
@@ -2,15 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./styles/main.scss";
 import App from "./components/app";
+import Admin from "./components/admin/admin";
 import { createLocalStorage } from "./utils/local-store";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter as Router, Route} from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch} from "react-router-dom";
 
 createLocalStorage();
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <Route path="/" component={App} />
+      <Switch>
+        <Route path="/admin" component={Admin} />
+        <Route path="/" component={App} />
+      </Switch>
     </Router>
   </React.StrictMode>,
   document.getElementById("root"),
